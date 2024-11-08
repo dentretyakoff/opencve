64d05b27025d87c5e5243e76fc6560ea3afe3a5f
Денис Третьяков <den.tretyakoff@yandex.ru>
2024-11-08 19:55:07 +0700

    cvss_metric

--- web/cves/serializers.py
+++ web/cves/serializers.py
@@ -6,18 +6,42 @@ from cves.utils import extract_product_info
 
 class BaseCveSerializer(serializers.ModelSerializer):
     nvd_versions = serializers.SerializerMethodField()
+    cvss_metric = serializers.SerializerMethodField()
 
     def get_nvd_versions(self, obj):
         return extract_product_info(obj.nvd_json)
 
+    def get_cvss_metric(self, obj):
+        return max(
+            obj.cvssV2_0.get("score", 0),
+            obj.cvssV3_0.get("score", 0),
+            obj.cvssV3_1.get("score", 0),
+            obj.cvssV4_0.get("score", 0),
+        )
+
     class Meta:
         model = Cve
-        fields = ["created_at", "updated_at", "cve_id", "description", "nvd_versions"]
+        fields = [
+            "created_at",
+            "updated_at",
+            "cve_id",
+            "description",
+            "nvd_versions",
+            "cvss_metric",
+            # "exploitation"
+        ]
 
 
 class CveListSerializer(BaseCveSerializer):
     class Meta(BaseCveSerializer.Meta):
-        fields = ["created_at", "updated_at", "cve_id", "description", "nvd_versions"]
+        fields = [
+            "created_at",
+            "updated_at",
+            "cve_id",
+            "description",
+            "nvd_versions",
+            "cvss_metric",
+        ]
 
 
 class CveDetailSerializer(BaseCveSerializer):
@@ -32,6 +56,7 @@ class CveDetailSerializer(BaseCveSerializer):
             "weaknesses",
             "vendors",
             "nvd_versions",
+            "cvss_metric",
         ]
 
 
